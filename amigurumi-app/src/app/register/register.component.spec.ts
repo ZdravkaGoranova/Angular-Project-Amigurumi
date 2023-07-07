63281a588123b2f92b65f0838358bb2b747ef83f
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-07-08 02:57:24 +0300

    add register component

--- amigurumi-app/src/app/register/register.component.spec.ts
+++ amigurumi-app/src/app/register/register.component.spec.ts
@@ -1,11 +1,4 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 
@@ -13,28 +6,16 @@ describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [RegisterComponent],
-      imports: [
-        NoopAnimationsModule,
-        ReactiveFormsModule,
-        MatButtonModule,
-        MatCardModule,
-        MatInputModule,
-        MatRadioModule,
-        MatSelectModule,
-      ]
-    }).compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RegisterComponent]
+    });
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should compile', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
--- amigurumi-app/src/app/register/register.component.ts
+++ amigurumi-app/src/app/register/register.component.ts
@@ -1,8 +1,4 @@
-import { Component, inject } from '@angular/core';
-
-
-import {FormControl, Validators, FormsModule, ReactiveFormsModule,FormBuilder} from '@angular/forms';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -10,27 +6,5 @@ import {FormControl, Validators, FormsModule, ReactiveFormsModule,FormBuilder} f
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  private fb = inject(FormBuilder);
-  addressForm = this.fb.group({
-    company: null,
-    firstName: [null, Validators.required],
-  
-    shipping: ['free', Validators.required]
-  });
 
-  email = new FormControl('', [Validators.required, Validators.email]);
-  hide = true;
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  onSubmit(): void {
-    alert('Thanks!');
-  }
-
-
-  
 }

56d9347e653f6ddfcf28efe3d3f5642dc1ec3ec8
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-07-08 02:58:11 +0300

    update routing element

--- amigurumi-app/src/app/app-routing.module.ts
+++ amigurumi-app/src/app/app-routing.module.ts
@@ -2,18 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   // { path: '/', component: MainComponent},
   // { path: '', pathMatch:'full', component: MainComponent},
 
-  // { path: 'login', component: LoginComponent },
-  // { path: 'register', component: RegisterComponent },
-  // { path: 'logout', component:},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
+  
+  { path: 'notFound', component: NotFoundComponent },
+  // { path: '**', component: NotFoundComponent },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
 
   { path: 'catalog', component: MainComponent },
 
-  // { path: 'Profile', component:},
+  { path: 'profile', component:UserProfileComponent},
   // { path: 'CONTACT', component:}
 
 ];
--- amigurumi-app/src/app/app.module.ts
+++ amigurumi-app/src/app/app.module.ts
@@ -8,18 +8,26 @@ import { CoreModule } from './core/core.module';
 import { ProductComponent } from './product/product.component';
 import { ProductsComponent } from './products/products.component';
 import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ProductsComponent,
-    MainComponent
+    MainComponent,
+    NotFoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+   
     CoreModule
   ],
   providers: [],

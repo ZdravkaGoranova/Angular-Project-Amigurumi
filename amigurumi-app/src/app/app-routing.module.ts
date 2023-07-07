e4a0499c97d9e77563f86ea222791e1170970742
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-07-07 16:56:14 +0300

    add routing

--- amigurumi-app/src/app/app-routing.module.ts
+++ amigurumi-app/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '/', component: MainComponent},
+  // { path: '', pathMatch:'full', component: MainComponent},
+
+  // { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  // { path: 'logout', component:},
+
+  { path: 'catalog', component: MainComponent },
+
+  // { path: 'Profile', component:},
+  // { path: 'CONTACT', component:}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- amigurumi-app/src/app/core/core.module.ts
+++ amigurumi-app/src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -11,7 +13,9 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    SharedModule,
   ],
   exports:
     [HeaderComponent,

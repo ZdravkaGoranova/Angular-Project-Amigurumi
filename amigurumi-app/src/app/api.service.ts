9dd9309fb4a751a14d68d5ecf894c9e34354c361
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-08-11 01:51:26 +0300

    update api servise

--- amigurumi-app/src/app/api.service.ts
+++ amigurumi-app/src/app/api.service.ts
@@ -7,6 +7,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 
 import { Observable, } from 'rxjs';
+import { ErrorService } from './core/error/error.service';
 
 
 @Injectable({
@@ -25,6 +26,7 @@ export class ApiService {
     private fireauth: AngularFireAuth,
     private router: Router,
     private firestore: Firestore,
+    private errorService: ErrorService,
 
   ) { }
 
@@ -45,19 +47,24 @@ export class ApiService {
   // }
 
   async getDataProducts(): Promise<Product[]> {
+    try {
+      const collectionInstance = collection(this.firestore, 'products');
+      const data = await collectionData(collectionInstance).toPromise();
 
-    const collectionInstance = collection(this.firestore, 'products');
-    const data = await collectionData(collectionInstance).toPromise();
-
-    if (data) {
-      const products: Product[] = data.map((docData) => {
-        const { id, ownerId, description, imageUrl, skillLevel, title, category, usersLiked, coments } = docData;
-        return { id, ownerId, description, imageUrl, skillLevel, title, category, usersLiked, coments };
-      });
-      return products;
-    } else {
+      if (data) {
+        const products: Product[] = data.map((docData) => {
+          const { id, ownerId, description, imageUrl, skillLevel, title, category, usersLiked, coments } = docData;
+          return { id, ownerId, description, imageUrl, skillLevel, title, category, usersLiked, coments };
+        });
+        return products;
+      } else {
+        return [];
+      }
+    } catch (error) {
+      this.errorService.setError(error);
       return [];
     }
+
     // getDataProducts() {
     //   const collectionInstance = collection(this.firestore, 'products');
     //   collectionData(collectionInstance).subscribe(val => {
@@ -84,58 +91,81 @@ export class ApiService {
   }
 
   async deleteProducts(id: string) {
-    const collectionInstance = collection(this.firestore, 'products');
 
-    await deleteDoc(doc(collectionInstance, id));
+    try {
+      const collectionInstance = collection(this.firestore, 'products');
 
+      await deleteDoc(doc(collectionInstance, id));
+    } catch (error) {
+      this.errorService.setError(error);
+
+    }
   }
   async getCurrentProduct(id: string): Promise<Product | null> {
-    const collectionInstance = collection(this.firestore, 'products');
 
-    const docRef = doc(collectionInstance, id);
-    const docSnap = await getDoc(docRef);
+    try {
 
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
+      const collectionInstance = collection(this.firestore, 'products');
 
-      const productData = docSnap.data();
+      const docRef = doc(collectionInstance, id);
+      const docSnap = await getDoc(docRef);
 
-      const product: Product = {
-        id: docSnap.id,
-        ownerId: productData['ownerId'],
-        description: productData['description'],
-        imageUrl: productData['imageUrl'],
-        skillLevel: productData['skillLevel'],
-        title: productData['title'],
-        category: productData['category'],
-        usersLiked: productData['usersLiked'],
-        coments: productData['coments']
-      };
-      console.log(product)
-      return product;
-    } else {
-      console.log("No such document!");
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+
+        const productData = docSnap.data();
+
+        const product: Product = {
+          id: docSnap.id,
+          ownerId: productData['ownerId'],
+          description: productData['description'],
+          imageUrl: productData['imageUrl'],
+          skillLevel: productData['skillLevel'],
+          title: productData['title'],
+          category: productData['category'],
+          usersLiked: productData['usersLiked'],
+          coments: productData['coments']
+        };
+        console.log(product)
+        return product;
+      } else {
+        console.log("No such document!");
+        return null;
+      }
+    } catch (error) {
+      this.errorService.setError(error);
       return null;
     }
+
   }
 
   async getCurrentProductOwner(id: string): Promise<string | null> {
-    const collectionInstance = collection(this.firestore, 'products');
-    const docRef = doc(collectionInstance, id);
-    const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-      const productData = docSnap.data();
-      const ownerId: string = productData['ownerId'];
-      console.log(ownerId)
-      return ownerId;
-    } else {
-      console.log("No such document!");
+    try {
+      const collectionInstance = collection(this.firestore, 'products');
+      const docRef = doc(collectionInstance, id);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const productData = docSnap.data();
+        const ownerId: string = productData['ownerId'];
+        console.log(ownerId)
+        return ownerId;
+      } else {
+        console.log("No such document!");
+        return null;
+      }
+
+    } catch (error) {
+      this.errorService.setError(error);
       return null;
     }
   }
 
-  // async getAll() {
+}
+
+
+ // async getAll() {
   //   const collectionInstance = collection(this.firestore, 'products');
 
   //   const querySnapshot = await getDocs(collectionInstance);
@@ -144,5 +174,3 @@ export class ApiService {
   //     console.log(doc.id, " => ", doc.data());
   //   });
   // }
-
-}
